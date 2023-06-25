16cd328baf77074a30073a23f9e064e6a3618ee6
ZexxySnowy <131374788+ZexxySnowy@users.noreply.github.com>
2023-06-25 18:45:42 +0530

    Update reporting.py

--- AloneRobot/modules/reporting.py
+++ AloneRobot/modules/reporting.py
@@ -274,7 +274,7 @@ __help__ = """
    • ɪғ ᴅᴏɴᴇ ɪɴ ᴘᴍ, ᴛᴏɢɢʟᴇs ʏᴏᴜʀ sᴛᴀᴛᴜs.
    • ɪғ ɪɴ ɢʀᴏᴜᴘ, ᴛᴏɢɢʟᴇs ᴛʜᴀᴛ ɢʀᴏᴜᴘs's sᴛᴀᴛᴜs.
 
-☆............𝙱𝚈 » [𝙰𝙻𝙾𝙽𝙴](https://t.me/ALONE_WAS_BOT)............☆
+☆............𝙱𝚈 » [VIRANYA](https://t.me/ViLLenOp09)............☆
 """
 
 SETTING_HANDLER = CommandHandler("reports", report_setting)
