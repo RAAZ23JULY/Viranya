7fafaadc3f0a1da1319c6835433a6d0cf686c7cf
ZexxySnowy <131374788+ZexxySnowy@users.noreply.github.com>
2023-06-25 20:30:24 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 
 @echo off
-TITLE Alone Robot
+TITLE Vianya Robot
 :: Enables virtual env mode and then starts Alone
-env\scripts\activate.bat && py -m AloneRobot
+env\scripts\activate.bat && py -m VianyaRobot
